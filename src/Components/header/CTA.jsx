79c0da95a48f886ec9cfc2d2ca4a683e2ff020c4
lalitCodes84 <vickysharma71za@gmail.com>
2024-08-06 12:39:41 +0530

    Up dated resume

--- src/Components/header/CTA.jsx
+++ src/Components/header/CTA.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="https://drive.google.com/uc?export=download&id=1ZBdbjNo-fK9lz89yimeWxR8Hestb-TUa" className='btn' target="_blank">My Resume</a>
+        <a href="https://drive.google.com/file/d/1rWnNuq_Whxbt8Z4_pBq9GYDrdmSuJVOZ/view?usp=sharing" className='btn' target="_blank">My Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
